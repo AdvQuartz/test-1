8f7ee2b17b3c0021ae926eadedc2cff196a0ce02
AdvQuartz <konstantin2835@gmail.com>
2025-10-01 21:10:26 +0300

    new

--- VS/mATCH GAME/mATCH GAME/MainWindow.xaml.cs
+++ VS/mATCH GAME/mATCH GAME/MainWindow.xaml.cs
@@ -5,18 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace mATCH_GAME
 
 {
-    using System.Windows.Threading;
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
